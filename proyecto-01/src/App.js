f8b2922ab959b49217a5795eb587acea43839a0d
Alberto Ramos Monagas <alberto.ramos@insendi.com>
2023-11-24 16:44:41 +0100

    deconstruction into components and refactor to create component

--- proyecto-01/src/App.js
+++ proyecto-01/src/App.js
@@ -1,3 +1,7 @@
+import { Crear } from "./components/Crear";
+import { Buscador } from "./components/Buscador";
+import { Listado } from "./components/Listado";
+
 function App() {
   return (
     <div className="layout">
@@ -31,57 +35,15 @@ function App() {
       {/* Contenido principal */}
       <section id="content" className="content">
         {/* aqui van las peliculas */}
-        <article className="peli-item">
-          <h3 className="title">Desarrollo web</h3>
-          <p className="description">victorroblesweb.es</p>
-
-          <button className="edit">Editar</button>
-          <button className="delete">Borrar</button>
-        </article>
-
-        <article className="peli-item">
-          <h3 className="title">Desarrollo web</h3>
-          <p className="description">victorroblesweb.es</p>
-
-          <button className="edit">Editar</button>
-          <button className="delete">Borrar</button>
-        </article>
-
-        <article className="peli-item">
-          <h3 className="title">Desarrollo web</h3>
-          <p className="description">victorroblesweb.es</p>
-
-          <button className="edit">Editar</button>
-          <button className="delete">Borrar</button>
-        </article>
-
-        <article className="peli-item">
-          <h3 className="title">Desarrollo web</h3>
-          <p className="description">victorroblesweb.es</p>
-
-          <button className="edit">Editar</button>
-          <button className="delete">Borrar</button>
-        </article>
+        <Listado />
       </section>
 
       {/* Barra lateral */}
       <aside className="lateral">
-        <div className="search">
-          <h3 className="title">Buscador</h3>
-          <form>
-            <input type="text" id="search_field" />
-            <button id="search">Buscar</button>
-          </form>
-        </div>
-
-        <div className="add">
-          <h3 className="title">Añadir pelicula</h3>
-          <form>
-            <input type="text" id="title" placeholder="Titulo" />
-            <textarea id="description" placeholder="Descripción"></textarea>
-            <input type="submit" id="save" value="Guardar" />
-          </form>
-        </div>
+        {/* Buscador */}
+        <Buscador />
+        {/* Añadir películas */}
+        <Crear />
       </aside>
 
       {/* Pie de página */}
---
+++ proyecto-01/src/components/Crear.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+export const Crear = () => {
+  const tituloComponente = "Añadir película";
+  const [peliState, setPeliState] = useState({ titulo: "", descripcion: "" });
+  const { titulo, descripcion } = peliState;
+
+  const conseguirDatosForm = (e) => {
+    // Para evitar el recargo de pantalla y que la aplicacion sea funcional y reactiva
+    e.preventDefault();
+    // Conseguir datos del formulario
+    let target = e.target; // Consigue el evento como tal
+    let titulo = target.titulo.value;
+    let descripcion = target.descripcion.value;
+    // Queremos guardar la informacion en un obj
+    let peli = {
+      id: new Date().getTime(), // Para siempre tener un id diferente
+      titulo,
+      descripcion,
+    };
+    // Queremos guardar esta información en el estado del componente
+    setPeliState(peli);
+  };
+
+  return (
+    <div className="add">
+      <h3 className="title">{tituloComponente}</h3>
+      <strong>
+        {titulo &&
+          descripcion &&
+          "Has añadido la película: " +
+            titulo +
+            " con descripción: " +
+            descripcion}
+      </strong>
+      <form onSubmit={conseguirDatosForm}>
+        <input type="text" name="titulo" id="title" placeholder="Titulo" />
+        <textarea
+          id="descripcion"
+          name="descripcion"
+          placeholder="Descripción"
+        ></textarea>
+        <input type="submit" id="save" value="Guardar" />
+      </form>
+    </div>
+  );
+};
