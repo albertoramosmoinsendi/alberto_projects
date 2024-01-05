1689ebc8ffd86f98239a638f2a8a28d4230cf139
Alberto Ramos Monagas <alberto.ramos@insendi.com>
2024-01-05 18:04:25 +0000

    Dando estilos a las páginas individuales

--- proyecto-03/src/components/Curriculum.js
+++ proyecto-03/src/components/Curriculum.js
@@ -3,7 +3,7 @@ import React from "react";
 export const Curriculum = () => {
   return (
     <div className="page">
-      <h1>Curriculum</h1>
+      <h1 className="heading">Curriculum</h1>
       <h2>Experiencia</h2>
       <h2>Conocimientos</h2>
       <h2>Proyectos personales</h2>
--- proyecto-03/src/components/Inicio.js
+++ proyecto-03/src/components/Inicio.js
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 
 export const Inicio = () => {
   return (
-    <div>
+    <div className="home">
       <h1>
-        Me especializo en ofrecer soluciones de software a medida diseñadas para
-        satisfacer las necesidades únicas de tu negocio. Con una pasión por la
-        innovación y un compromiso con la excelencia, ofrezco una gama de
-        servicios de desarrollo para impulsar la eficiencia, productividad y
-        crecimiento de tu proyecto u organización.
+        Me especializo en ofrecer soluciones de
+        <strong> software a medida diseñadas </strong> para satisfacer las
+        necesidades únicas de tu negocio. Con una pasión por la innovación y un
+        compromiso con la excelencia, ofrezco una gama de servicios de
+        desarrollo para impulsar la eficiencia, productividad y crecimiento de
+        tu proyecto u organización.
       </h1>
 
       <h2>
@@ -18,7 +19,7 @@ export const Inicio = () => {
       </h2>
 
       <section className="last-works">
-        <h2>Algunos de mis proyectos</h2>
+        <h2 className="heading">Algunos de mis proyectos</h2>
         <p>Estos son algunos de mis trabajos de desarrollo web</p>
 
         <div className="works">{/* Trabajos a hacer */}</div>
