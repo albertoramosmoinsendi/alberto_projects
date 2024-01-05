fdd931d36d0a6d135889f9a270ca6d4600da3153
Alberto Ramos Monagas <alberto.ramos@insendi.com>
2024-01-05 18:56:40 +0000

    Creando array de objetos de proyectos

--- proyecto-03/src/components/Portafolio.js
+++ proyecto-03/src/components/Portafolio.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { trabajos } from "../data/trabajos";
 
 export const Portafolio = () => {
   return (
     <div className="page">
       <h1 className="heading">Portafolio</h1>
+
+      {trabajos.map((trabajo) => {
+        <article>
+          <h2>{trabajo.nombre}</h2>
+        </article>;
+      })}
     </div>
   );
 };
---
+++ proyecto-03/src/data/trabajos.js
@@ -0,0 +1,44 @@
+export const trabajos = [
+  {
+    id: "insendi",
+    nombre: "studygroup",
+    url: "insendi.com",
+    tecnologias: "react, js, ts, etc",
+    categorias: "diseño y desarrollo web",
+  },
+  {
+    id: "educaciononline",
+    nombre: "EduOnline",
+    url: "https://www.educaciononline.com",
+    tecnologias: "Node.js, MongoDB, React, Redux",
+    categorias: "Educación, Desarrollo Web",
+  },
+  {
+    id: "vidasaludable",
+    nombre: "Vida Saludable",
+    url: "https://vidasaludable.com",
+    tecnologias: "Vue.js, Firebase, HTML, CSS",
+    categorias: "Salud, Nutrición, Desarrollo Web",
+  },
+  {
+    id: "tiendaonline",
+    nombre: "MiTienda",
+    url: "https://www.mitiendaonline.com",
+    tecnologias: "Angular, Node.js, MongoDB",
+    categorias: "Comercio electrónico, Desarrollo Web",
+  },
+  {
+    id: "projectmanager",
+    nombre: "ProjectManager",
+    url: "https://projectmanagerapp.com",
+    tecnologias: "Django, React, PostgreSQL",
+    categorias: "Gestión de Proyectos, Desarrollo Web",
+  },
+  {
+    id: "travelblog",
+    nombre: "TravelDiaries",
+    url: "https://www.traveldiaries.com",
+    tecnologias: "WordPress, PHP, MySQL",
+    categorias: "Viajes, Blogging, Desarrollo Web",
+  },
+];
