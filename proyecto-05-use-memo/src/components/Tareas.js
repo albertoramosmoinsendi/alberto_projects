9125e5e311a61d8368f79972a70cd05d391337ea
Alberto Ramos Monagas <alberto.ramos@insendi.com>
2024-01-19 15:07:27 +0100

    el hook useMemo parte 2 aplicacion de tareas

--- proyecto-05-use-memo/src/components/Tareas.js
+++ proyecto-05-use-memo/src/components/Tareas.js
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 export const Tareas = () => {
+  const [tareas, setTareas] = useState([]);
+
+  const guardarTareas = (e) => {
+    e.preventDefault();
+    let tareas_actualizadas = [...tareas, e.target.descripcion.value];
+    // Expandimos el estado de tareas más un nuevo elemento
+    setTareas(tareas_actualizadas);
+  };
+
   return (
     <div className="tareas-container">
       <h1>Mis tareas</h1>
-      <form>
+      <form onSubmit={guardarTareas}>
         <input type="text" name="descripcion" placeholder="Describe la tarea" />
         <input type="submit" value="Guardar" />
       </form>
 
       <h3>Lista de tareas:</h3>
+      <ul>
+        {tareas.map((tarea, index) => {
+          return <li key={index}>{tarea}</li>;
+        })}
+      </ul>
     </div>
   );
 };
